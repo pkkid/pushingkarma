479b3d353582ba1f34a7028e1b3c24e51ab6e02d
Michael Shepanski <mjs7231@gmail.com>
2015-09-05 00:21:11 -0400

    Copy over less files from old pushingkarma

---
+++ pk/static/js/init.js
@@ -0,0 +1,11 @@
+/*----------------------------------------------------------
+ * Copyright (c) 2015 PushingKarma. All rights reserved.
+ *------------------------------------------------------- */
+'use strict';
+
+(function() {
+    // Register Tooltips
+    $("a[data-tooltip]").tooltip({delay:{show:200, hide:50}});
+    // Apply Prettyprint to code blocks
+    $(".content pre").prettyPrint();
+})();
