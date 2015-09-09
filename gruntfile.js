22b17594f121cc6280cf84038a38ae83c6f3238f
Michael Shepanski <mjs7231@gmail.com>
2015-09-09 09:54:48 -0400

    Update README

--- gruntfile.js
+++ gruntfile.js
@@ -58,4 +58,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['concat', 'sass']);
 };
