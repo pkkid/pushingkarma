e434c5e51f605b3eeb76fab0166e05d6a0b53c7a
Michael Shepanski <mjs7231@gmail.com>
2015-09-09 23:19:25 -0400

    Sideboxes and Thumbnails working

--- pk/views/projects.py
+++ pk/views/projects.py
@@ -7,25 +7,25 @@ from pk.utils import context, response
 
 
 def overview(request, template='projects/projects.html'):
-    data = context.core(menuitem='projects')
+    data = context.core(request, menuitem='projects')
     return response(request, template, data)
 
 
 def django_dbbackup(request, template='projects/django_dbbackup.html'):
-    data = context.core(menuitem='projects')
+    data = context.core(request, menuitem='projects')
     return response(request, template, data)
 
 
 def jquery_wysiwym(request, template='projects/jquery_wysiwym.html'):
-    data = context.core(menuitem='jquery_wysiwym')
+    data = context.core(request, menuitem='jquery_wysiwym')
     return response(request, template, data)
 
 
 def pkmeter(request, template='projects/pkmeter.html'):
-    data = context.core(menuitem='jquery_wysiwym')
+    data = context.core(request, menuitem='jquery_wysiwym')
     return response(request, template, data)
 
 
 def pks_movie_database(request, template='projects/pks_movie_database.html'):
-    data = context.core(menuitem='jquery_wysiwym')
+    data = context.core(request, menuitem='jquery_wysiwym')
     return response(request, template, data)
