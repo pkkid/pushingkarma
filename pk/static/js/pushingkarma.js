41b9b221083d6578b40a31b53ea5c10630633f8e
Michael Shepanski <mjs7231@gmail.com>
2015-09-28 23:33:04 -0400

    Add CodeMirror editor with popput transition

--- pk/static/js/pushingkarma.js
+++ pk/static/js/pushingkarma.js
@@ -126,6 +126,24 @@ pk.login_form = {
 'use strict';
 
 (function() {
+
+    var init_editor = function() {
+        $('.page-editor .handle').on('click', function() {
+            $('#layoutborder').toggleClass('editing');
+        });
+        CodeMirror.fromTextArea(document.getElementById('page-textarea'), {
+            lineNumbers: true,
+            mode: 'htmlmixed',
+            theme: 'blackboard',
+            scrollbarStyle: 'simple',
+        });
+    };
+
+    setTimeout(function() {
+        $('body').removeClass('preload');
+    }, 500);
+
     pk.login_form.init();
     pk.utils.init_tooltips();
+    init_editor();
 })();
