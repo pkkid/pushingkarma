a12926596539946d3cd05dab8220777a2f7566be
Michael Shepanski <mjs7231@gmail.com>
2015-11-23 23:25:04 -0500

    Move JS

--- pk/static/js/notebook.js
+++ pk/static/js/notebook.js
@@ -4,10 +4,19 @@
 'use strict';
 
 pk.notebook = {
+    EDITOR_SELECTOR: '#notebook-editor',
+    NOTE_SELECTOR: '#note',
 
     init: function(selector) {
         this.container = $(selector);
         console.debug('init pk.notebook: '+ selector);
+        this.init_editor();
+    },
+
+    init_editor: function() {
+        pk.editor.init(this.EDITOR_SELECTOR, {
+            output: this.NOTE_SELECTOR,
+        });
     },
 
 };
