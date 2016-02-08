3fecdb351d0f4e295ca605110a0cd835908c761f
Michael Shepanski <mjs7231@gmail.com>
2016-02-08 00:57:32 -0500

    Move env to conf (its moreexplicit); Add nginx configuration files

--- conf/dbbackup-cron.sh
+++ conf/dbbackup-cron.sh
@@ -1,4 +1,6 @@
+# Django DBBackup script to run via cron
 # Copyright (c) 2015 PushingKarma. All rights reserved.
+# @daily /home/mjs7231/Projects/pushingkarma/conf/dbbackup-cron.sh >> /home/mjs7231/Logs/pushingkarma/dbbackup.log 2>&1
 cd /home/mjs7231/Projects/pushingkarma
 export PYTHONPATH="/home/mjs7231/Projects/pushingkarma:$PYTHONPATH"
 . /home/mjs7231/.virtualenvs/pushingkarma/bin/activate
