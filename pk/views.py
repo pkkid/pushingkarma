2235537cce15f5901f2bff6515c02b339b86b585
Michael Shepanski <mjs7231@gmail.com>
2017-02-12 12:03:25 -0500

    Fix searching if no slug

--- pk/views.py
+++ pk/views.py
@@ -4,14 +4,21 @@
 Copyright (c) 2015 PushingKarma. All rights reserved.
 """
 from django.http import HttpResponse
+from pk.api import NOTESEARCHFIELDS
 from pk.models import Note, Page
 from pk.serializers import NoteSerializer, PageSerializer
+from pk.utils.search import Search
 from pk.utils import get_object_or_none, context, response, response_json
 from pk.utils.markdown import Markdown
 
 
 def note(request, slug=None, template='note.html'):
-    note = get_object_or_none(Note, slug=slug) or Note.objects.order_by('-modified')[0]
+    notes = Note.objects.order_by('-modified')
+    slugnote = get_object_or_none(Note, slug=slug)
+    search = request.GET.get('search')
+    if slugnote: note = slugnote
+    elif search: note = Search(notes, NOTESEARCHFIELDS, search).queryset()[0]
+    else: note = notes[0]
     data = context.core(request, menuitem='notebook')
     data.note = NoteSerializer(note, context={'request':request}).data
     return response(request, template, data)
