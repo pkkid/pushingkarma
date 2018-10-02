300d77faac3f820e57e410f6d75cd840b98a225b
Michael Shepanski <mjs7231@gmail.com>
2018-10-02 16:46:50 -0400

    Started work on gauth

--- pk/apps/raspi/views.py
+++ pk/apps/raspi/views.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import requests
+from googleapiclient.discovery import build
 from django.conf import settings
 from pk import log, utils
 from pk.apps.calendar.views import get_events
@@ -13,6 +14,7 @@ def raspi(request, tmpl='raspi.html'):
     data.weather = _get_weather()
     data.calendar = _get_calendar()
     data.news = _get_news()
+    data.tasks = _get_tasks()
     # import pprint; pprint.pprint(data.weather)
     return utils.response(request, tmpl, data)
 
@@ -31,6 +33,17 @@ def _get_calendar():
     try:
         response = get_events(settings.RASPI_CALENDAR_URL)
         return response
+    except Exception as err:
+        log.exception(err)
+
+
+@cached(key='raspi-tasks')
+def _get_tasks():
+    try:
+        service = discovery.build('tasks', 'v1', developerKey=settings.GOOGLE_KEY)
+        results = service.tasklists().list(maxResults=99).execute()
+        items = results.get('items', [])
+        import pprint; pprint.pprint(results)
     except Exception as err:
         log.exception(err)
 
