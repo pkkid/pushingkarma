399ac8c542e450ea0f2cda2d5842b0ba758ada19
Michael Shepanski <mikes@nasuni.com>
2019-05-09 23:59:43 -0400

    Checkpoint

--- pk/static/site/js/app.jsx
+++ pk/static/site/js/app.jsx
@@ -1,5 +1,10 @@
-
-ReactDOM.render(
-    <h1>Sherlock Holmes</h1>,
-    document.body
-);
+var React = require('react');
+var ReactDOM = require('react-dom');
+ 
+class MyComponent extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+ 
+ReactDOM.render(<MyComponent />, node);
