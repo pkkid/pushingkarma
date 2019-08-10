c7f98d81a0c14f2a60204a6f059bc9765dce4841
Michael Shepanski <mikes@nasuni.com>
2019-08-10 14:12:04 -0400

    Fix capitalization

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import store from './store'
-import router from './router'
 import App from './App.vue'
+import Vue from 'vue'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
