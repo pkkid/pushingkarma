7c0604e9759155537fe9538aba7b754d02213c80
Michael Shepanski <mikes@nasuni.com>
2019-09-03 08:44:56 -0400

    Fix syntax error.

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
-import path from 'path';
-
 module.exports = {
   lintOnSave: true,
   publicPath: '/static/',
-  resolve: {
-    alias : {
-      'icons': path.resolve('@/node_modules/vue-material-design-icons')
-    }
-  }
 };
