11ad6aee122d6d859d89bd4ba8e4d511c839608e
Michael Shepanski <mikes@nasuni.com>
2019-09-03 23:11:26 -0400

    Basic styling for buttons.

--- src/components/notes/Notes.vue
+++ src/components/notes/Notes.vue
@@ -6,21 +6,21 @@
       <div class='note'>
         <editor-menu-bar :editor="editor" v-slot="{commands, isActive, focused}">
           <div class="menubar is-hidden" :class="{'is-focused': focused}">
-            <button class='menubar_button' :class='{"is-active":isActive.bold()}' @click='commands.bold'><i class='mdi mdi-format-bold'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.italic()}' @click='commands.italic'><i class='mdi mdi-format-italic'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.underline()}' @click='commands.underline'><i class='mdi mdi-format-underline'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.code()}' @click='commands.code'><i class='mdi mdi-code-not-equal-variant'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.paragraph()}' @click='commands.paragraph'><i class='mdi mdi-format-paragraph'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.heading({level:1})}' @click='commands.heading({level:1})'><i class='mdi mdi-format-header-1'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.heading({level:2})}' @click='commands.heading({level:2})'><i class='mdi mdi-format-header-2'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.heading({level:3})}' @click='commands.heading({level:3})'><i class='mdi mdi-format-header-3'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.bullet_list()}' @click='commands.bullet_list'><i class='mdi mdi-format-list-bulleted'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.ordered_list()}' @click='commands.ordered_list'><i class='mdi mdi-format-list-numbered'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.blockquote()}' @click='commands.blockquote'><i class='mdi mdi-format-quote-close'></i></button>
-            <button class='menubar_button' :class='{"is-active":isActive.code_block()}' @click='commands.code_block'><i class='mdi mdi-code-tags'></i></button>
-            <button class='menubar_button'><i class='mdi mdi-file-code-outline'></i></button>
-            <button class='menubar_button' @click='commands.undo'><i class='mdi mdi-undo'></i></button>
-            <button class='menubar_button' @click='commands.redo'><i class='mdi mdi-redo'></i></button>
+            <button :class='{"active":isActive.bold()}' @click='commands.bold'><i class='mdi mdi-format-bold'></i></button>
+            <button :class='{"active":isActive.italic()}' @click='commands.italic'><i class='mdi mdi-format-italic'></i></button>
+            <button :class='{"active":isActive.underline()}' @click='commands.underline'><i class='mdi mdi-format-underline'></i></button>
+            <button :class='{"active":isActive.code()}' @click='commands.code'><i class='mdi mdi-code-not-equal-variant'></i></button>
+            <button :class='{"active":isActive.paragraph()}' @click='commands.paragraph'><i class='mdi mdi-format-paragraph'></i></button>
+            <button :class='{"active":isActive.heading({level:1})}' @click='commands.heading({level:1})'><i class='mdi mdi-format-header-1'></i></button>
+            <button :class='{"active":isActive.heading({level:2})}' @click='commands.heading({level:2})'><i class='mdi mdi-format-header-2'></i></button>
+            <button :class='{"active":isActive.heading({level:3})}' @click='commands.heading({level:3})'><i class='mdi mdi-format-header-3'></i></button>
+            <button :class='{"active":isActive.bullet_list()}' @click='commands.bullet_list'><i class='mdi mdi-format-list-bulleted'></i></button>
+            <button :class='{"active":isActive.ordered_list()}' @click='commands.ordered_list'><i class='mdi mdi-format-list-numbered'></i></button>
+            <button :class='{"active":isActive.blockquote()}' @click='commands.blockquote'><i class='mdi mdi-format-quote-close'></i></button>
+            <button :class='{"active":isActive.code_block()}' @click='commands.code_block'><i class='mdi mdi-code-tags'></i></button>
+            <button><i class='mdi mdi-file-code-outline'></i></button>
+            <button @click='commands.undo'><i class='mdi mdi-undo'></i></button>
+            <button @click='commands.redo'><i class='mdi mdi-redo'></i></button>
           </div>
         </editor-menu-bar>
         <editor-content :editor='editor' />
@@ -87,6 +87,18 @@
       width: 800px;
       margin: 20px auto;
       min-height: calc(100vh - 70px);
+    }
+
+    button {
+      border: 0px;
+      background-color: transparent;
+      margin-right: 5px;
+      border-radius: 5px;
+      font-size: 20px;
+      color: #333;
+      cursor: pointer;
+      &:hover { background-color: #ddd; }
+      &.active { background-color: #ccc; }
     }
   }
 </style>
