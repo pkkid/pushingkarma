47dfe335f65e28921cbd39e6998736da64229382
Michael Shepanski <mikes@nasuni.com>
2019-09-30 22:14:03 -0400

    Add FontColor extension

--- src/utils/tiptap-extensions.js
+++ src/utils/tiptap-extensions.js
@@ -30,20 +30,49 @@ export class FontSize extends Mark {
         style: 'font-size',
         getAttrs: value => value.indexOf('em') !== -1 ? {fontSize:value} : '',
       }],
-      toDOM: mark => ['span', {style:`font-size: ${mark.attrs.fontSize}`}, 0],
+      toDOM: mark => ['span', {style:`font-size:${mark.attrs.fontSize}`}, 0],
     };
   }
 
   commands({type}) {
     return attrs => {
-      if ((attrs.fontSize) && (attrs.fontSize != '1em')) {
-        return updateMark(type, attrs);
-      }
-      return removeMark(type);
+      return attrs.fontSize ? updateMark(type, attrs) : removeMark(type);
     };
   }
   
   inputRules({type}) {
     return [markInputRule(/(?:\*\*|__)([^*_]+)(?:\*\*|__)$/, type)];
   }
+}
+
+
+/**
+ * FontColor - Extends the built in Mark from tiptap allowing
+ * the editor to set the font color.
+ */
+export class FontColor extends Mark {
+	get name() {
+		return 'fontColor';
+	}
+
+	get schema() {
+		return {
+			attrs: {fontColor: {default: '#000000'}},
+			parseDOM: [{
+				style: 'color',
+				getAttrs: mark => mark.indexOf('rgb') !== -1 ? {fontColor: mark} : '',
+			}],
+			toDOM: mark => ['span', {style:`color:${mark.attrs.fontColor}`}, 0],
+		};
+	}
+	
+	commands({type}) {
+    return attrs => {
+      return attrs.fontColor ? updateMark(type, attrs) : removeMark(type);
+    };
+  }
+
+	inputRules({ type }) {
+		return [markInputRule(/(?:\*\*|__)([^*_]+)(?:\*\*|__)$/, type)];
+	}
 }
