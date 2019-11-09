d5f6000b7883d05e35398a8fcff2611bff70abd7
Michael Shepanski <mikes@nasuni.com>
2019-11-09 00:43:37 -0500

    Show transactions

--- src/components/budget/BudgetTransactions.vue
+++ src/components/budget/BudgetTransactions.vue
@@ -1,6 +1,28 @@
 <template>
   <div id='budgetmonth'>
     Budget Transactions {{account ? account.name : 'ALL'}}
+    <table cellpadding='0' cellspacing='0'>
+      <thead><tr>
+        <th class='account'>Bank</th>
+        <th class='date'>Date</th>
+        <th class='payee'>Payee</th>
+        <th class='category'>Category</th>
+        <th class='amount'>Amount</th>
+        <th class='approved'>X</th>
+        <th class='comment'>Comment</th>
+      </tr></thead>
+      <tbody>
+        <tr v-for='trx in transactions' :key='trx.id'>
+          <td class='account'><div>{{trx.account.name}}</div></td>
+          <td class='date editable'><div>{{trx.date}}</div></td>
+          <td class='payee editable selectall'><div>{{trx.payee}}</div></td>
+          <td class='category editable selectall'><div>{{trx.category ? trx.category.name : ''}}</div></td>
+          <td class='amount float'><div>{{trx.amount}}</div></td>
+          <td class='approved bool selectall'><div>X</div></td>
+          <td class='comment editable'><div>{{trx.comment}}</div></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
