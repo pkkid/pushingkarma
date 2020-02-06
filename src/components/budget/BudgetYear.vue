a4639bf009de5253eb6834a250e36715688287fd
Michael Shepanski <mikes@nasuni.com>
2020-02-05 23:02:45 -0500

    Checkpoint: Iterate categories

--- src/components/budget/BudgetYear.vue
+++ src/components/budget/BudgetYear.vue
@@ -2,10 +2,30 @@
   <div id='budgetmonth'>
     <h3>Past Year Budget</h3>
     <div class='tablewrap'>
-      Hi Dad!
-      <div v-for='group of groups' :key='group.id'>
-
-      </div>
+      <table cellpadding='0' cellspacing='0'>
+        <thead><tr>
+          <th class='category'><div>Category</div></th>
+          <th class='trend'><div>Trend</div></th>
+          <th class='budget'><div>Budget</div></th>
+          <th class='month' v-for='month in months' :key='month.format("YYYY-MM")'>
+            <div>{{month.format("MMM")}}</div>
+          </th>
+          <th class='average'><div>Average</div></th>
+          <th class='total'><div>Total</div></th>
+        </tr></thead>
+        <tbody>
+          <tr v-for='cat in this.categories' :key='"cat-"+cat.id'>
+            <td class='category'><div>{{cat.name}}</div></td>
+            <td class='trend'><div>--</div></td>
+            <td class='budget'><div>{{cat.budget}}</div></td>
+            <td class='month' v-for='(trxs,monthstr) in groups[cat.name]' :key='cat.name+monthstr'>
+              <div>{{monthstr}}</div>
+            </td>
+            <td class='average'><div>--</div></td>
+            <td class='total'><div>--</div></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -20,7 +40,7 @@
     name: 'BudgetYear',
     data: () => ({
       transactions: {},                   // Displayed transactions
-      groups: [],                         // Grouped Transactions
+      groups: {},                         // Grouped Transactions
       start: moment().startOf('month'),   // Starting month
       uncategorized: 'Uncategorized',     // Uncategorized label
     }),
@@ -31,6 +51,15 @@
     },
     computed: {
       categories: pathify.sync('budget/categories'),
+      months: function() {
+        var months = [];
+        var month = this.start.clone();
+        for (var i=0; i<=12; i++) {
+          months.push(month.clone());
+          month.subtract(1, 'months');
+        }
+        return months;
+      },
     },
     methods: {
       // Update Transactions
@@ -52,14 +81,9 @@
         var catnames = _.map(this.categories, 'name');
         catnames.push(this.uncategorized);
         for (var catname of catnames) {
-          var month = this.start.clone();
-          var monthstr = month.format('YYYY-MM-DD');
           groups[catname] = {};
-          groups[catname][monthstr] = [];
-          for (var i=0; i<12; i++) {
-            month.subtract(1, 'months');
-            monthstr = month.format('YYYY-MM-DD');
-            groups[catname][monthstr] = [];
+          for (var month of this.months) {
+            groups[catname][month.format('YYYY-MM')] = [];
           }
         }
         return groups;
@@ -72,7 +96,7 @@
         for (var trx of this.transactions) {
           var catname = trx.category ? trx.category.name : this.uncategorized;
           var month = moment(trx.date).startOf('month');
-          var monthstr = month.format('YYYY-MM-DD');
+          var monthstr = month.format('YYYY-MM');
           groups[catname][monthstr].push(trx);
         }
         this.groups = groups;
