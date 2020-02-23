968976ebada51f3d7be54558a8fe8b3822f3668a
Michael Shepanski <mikes@nasuni.com>
2020-02-22 22:33:11 -0500

    New read-only api views.

--- pk/urls.py
+++ pk/urls.py
@@ -9,6 +9,7 @@ from pk.apps.notes import api as note_api
 from pk.apps.stocks import views as stock_views
 from pk.utils.api import HybridRouter
 
+# Basic DefaultRouter
 api = HybridRouter(trailing_slash=False)
 api.register('user', pk_api.AccountViewSet)
 api.register('notes', note_api.NotesViewSet)
@@ -17,7 +18,13 @@ api.register('categories', budget_api.CategoriesViewSet)
 api.register('transactions', budget_api.TransactionsViewSet)
 api.register('stocks', stock_views.StocksViewSet)
 api.register('keyval', budget_api.KeyValueViewSet)
-api.add_url(url(r'^hello', newtab_api.hello_world, name='hello'))
+
+# Read only views
+api.add_url(url(r'^calendar', newtab_api.calendar, name='calendar'))
+api.add_url(url(r'^news', newtab_api.news, name='news'))
+api.add_url(url(r'^photo', newtab_api.photo, name='photo'))
+api.add_url(url(r'^tasks', newtab_api.tasks, name='tasks'))
+api.add_url(url(r'^weather', newtab_api.weather, name='weather'))
 
 
 @ensure_csrf_cookie
