871220a30139c27dce88d516d6c223d6522fab51
Michael Shepanski <mikes@nasuni.com>
2020-03-08 17:57:22 -0400

    Checkpoint: Updating ansible to 2020.

---
+++ ansible/callback_plugins/check_resources.py
@@ -0,0 +1,14 @@
+# Checks the Secrets file is mounted
+from os.path import dirname, isfile, join
+from ansible.plugins.callback import CallbackBase
+
+PROJECT_DIR = dirname(dirname(dirname(__file__)))
+
+class CallbackModule(CallbackBase):
+    def __init__(self, *args, **kwargs):
+        secrets = join(PROJECT_DIR, 'pk/settings/secrets.py')
+        dist = join(PROJECT_DIR, 'dist/index.html')
+        if not isfile(secrets):
+            raise Exception(f'Secrets not present: {secrets}')
+        if not isfile(dist):
+            raise Exception(f'Distribution files not present: {dist}')
