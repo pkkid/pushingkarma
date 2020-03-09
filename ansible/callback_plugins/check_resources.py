4d1bd7be63067d8a3deb3129d28474b0200fd6e2
Michael Shepanski <mikes@nasuni.com>
2020-03-08 23:13:29 -0400

    Checkpoint: Trying to get asgi server running.

--- ansible/callback_plugins/check_resources.py
+++ ansible/callback_plugins/check_resources.py
@@ -7,7 +7,7 @@ PROJECT_DIR = dirname(dirname(dirname(__file__)))
 class CallbackModule(CallbackBase):
     def __init__(self, *args, **kwargs):
         secrets = join(PROJECT_DIR, 'pk/settings/secrets.py')
-        dist = join(PROJECT_DIR, 'dist/index.html')
+        dist = join(PROJECT_DIR, 'pk/_dist/index.html')
         if not isfile(secrets):
             raise Exception(f'Secrets not present: {secrets}')
         if not isfile(dist):
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,10 @@
 module.exports = {
-  lintOnSave: true,
-  outputDir: 'pk/_dist',
-  assetsDir: 'pushingkarma',
-  publicPath: '/static/',
-  runtimeCompiler: true,
-  css: {loaderOptions: {sass: {data: `@import "@/assets/css/__variables.scss";`}}},
-  chainWebpack: config => { config.plugins.delete("hmr"); },
-  configureWebpack: {
-    performance: {
-      maxEntrypointSize: 1024000,
-      maxAssetSize: 1024000
-    }
-  }
+  lintOnSave: true,             // Enabled https://cli.vuejs.org/config/#lintonsave
+  outputDir: 'pk/_dist',        // Save distribution files to pk dir
+  assetsDir: 'pushingkarma',    // Put frontend assets into a pushingkarma dir
+  publicPath: '/static/',       // Assets will be served at /static/
+  runtimeCompiler: true,        // Enable using templates in javascript files
+  css: {loaderOptions: {sass: {data: `@import "@/assets/css/__variables.scss";`}}},   // All components get variables by default
+  //chainWebpack: config => { config.plugins.delete("hmr"); },                        // Disable hot-reload plugin
+  configureWebpack: {performance: {maxEntrypointSize:1024000, maxAssetSize:1024000}}  // Remove warnings for large js files large
 };
