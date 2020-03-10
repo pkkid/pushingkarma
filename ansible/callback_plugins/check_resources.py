95ae6d1e41e19e2f1988c35fd1761a006776d005
Michael Shepanski <mikes@nasuni.com>
2020-03-09 23:18:46 -0400

    Cleanup checks.

--- ansible/callback_plugins/check_resources.py
+++ ansible/callback_plugins/check_resources.py
@@ -6,9 +6,17 @@ PROJECT_DIR = dirname(dirname(dirname(__file__)))
 
 class CallbackModule(CallbackBase):
     def __init__(self, *args, **kwargs):
+        # Check secrets file is present
         secrets = join(PROJECT_DIR, 'pk/settings/secrets.py')
-        dist = join(PROJECT_DIR, 'pk/_dist/index.html')
         if not isfile(secrets):
-            raise Exception(f'Secrets not present: {secrets}')
+            raise Exception(f'Secrets not present.\nPlease run: "addkeys.sh"')
+        # Check Distribution files are present
+        dist = join(PROJECT_DIR, 'pk/_dist/index.html')
         if not isfile(dist):
-            raise Exception(f'Distribution files not present: {dist}')
+            raise Exception(f'Development files present.\n'
+                'Please stop the dev server and run: "npm run build"')
+        # Check _dist directory contains production files
+        appjs = join(PROJECT_DIR, 'pk/_dist/pushingkarma/js/app.js')
+        if isfile(appjs):
+            raise Exception(f'Distribution files not present.\n'
+                'Please stop the dev server and run: "npm run build"')
