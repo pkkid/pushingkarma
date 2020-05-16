088a74ffd43f5e5f345b1dadfe6389359437b6e4
Michael Shepanski <mikes@nasuni.com>
2020-05-15 23:12:51 -0400

    Better way to force bgimage.

--- src/components/newtab/NewTabPhoto.vue
+++ src/components/newtab/NewTabPhoto.vue
@@ -1,20 +1,25 @@
 <template>
   <div id='photo' :class='{showdetails}'>
-    <!-- Removed backgroundImage:bgimage below -->
-    <div class='bgimg' :style='{opacity:bgopacity}'/>
+    <div class='bgimg' :style='{backgroundImage:bgimage, opacity:bgopacity}'/>
     <div class='details' v-if='photo'>
       <div class='title'><span v-if='photo.title'>{{photo.title}} |</span> {{photo.user}}</div>
       <div class='description'>
-        {{photo.description}} -
-        <a href='javascript:void(0)' @click.prevent='refreshPhoto'>New Photo</a>
+        {{photo.description}}
+        <span v-if='!forced'> - <a @click.prevent='refreshPhoto'>New Photo</a></span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  // import * as api from '@/api';
+  import * as api from '@/api';
   import * as utils from '@/utils/utils';
+  var FORCE_IMAGE = {
+    url: require('../../assets/img/darkbg.jpg'),
+    title: 'Black Floral Wallpaper',
+    description: 'Black Floral Wallpaper 1920x1080',
+    user: 'Unknown',
+  };
 
   export default {
     name: 'NewTabPhoto',
@@ -23,29 +28,32 @@
     },
     data: () => ({
       bgopacity: 0,
+      forced: false,
       photo: null,
       showdetails: false,
     }),
     mounted: async function() {
       // Removed: Hard coded bg image below
-      // var {data} = await api.Tools.getPhoto();
-      // this.photo = data;
-      this.photo = {
-        title: 'Black Floral Wallpaper',
-        description: 'Black Floral Wallpaper 1920x1080',
-      };
+      if (FORCE_IMAGE !== null) {
+        this.photo = FORCE_IMAGE;
+        this.forced = true;
+      } else {
+        var {data} = await api.Tools.getPhoto();
+        this.photo = data;
+      }
       await utils.preloadImage(this.photo.url);
       this.bgopacity = 1;
     },
     methods: {
       refreshPhoto: async function() {
-        // Removed: Hard coded bg image below
-        // this.bgopacity = 0;
-        // await utils.sleep(500);
-        // var {data} = await api.Tools.refreshPhoto();
-        // this.photo = data;
-        // await utils.preloadImage(this.photo.url);
-        // this.bgopacity = 1;
+        if (FORCE_IMAGE !== null) {
+          this.bgopacity = 0;
+          await utils.sleep(500);
+          var {data} = await api.Tools.refreshPhoto();
+          this.photo = data;
+          await utils.preloadImage(this.photo.url);
+          this.bgopacity = 1;
+        }
       },
       toggleDetails: function() {
         this.showdetails = !this.showdetails;
@@ -60,8 +68,6 @@
       width: 100%;
       height: 100vh;
       opacity: 0;
-      // Added hard coded background-image below
-      background-image: url('../../assets/img/darkbg.jpg');
       background-position: center center;
       background-size: cover;
       z-index: 0;
