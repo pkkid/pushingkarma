d1a11d9d64df9892b59ed229131eaf484e5c9a9c
Michael Shepanski <mikes@nasuni.com>
2021-08-24 14:37:40 -0400

    Cleanup data flow a bit. Still confused on adding a chart.

--- src/components/budget/BudgetMonth.vue
+++ src/components/budget/BudgetMonth.vue
@@ -7,26 +7,29 @@
           <i class='mdi mdi-chevron-right' @click.prevent='month = month.add(1, "month")'/>
           <b-button :class='{"active":month != today}' @click.prevent='month = today'>Today</b-button>
         </div>
-        <div class='subtext'>View {{count}} {{month.format('MMMM')}} transactions</div>
+        <div class='subtext'>View {{loading ? '' : count}} {{month.format('MMMM')}} transactions</div>
       </h1>
       <div style='clear:both'/>
       <div v-if='!loading' v-hotkey='keymap'>
-        <div id='monthdetails'>
-          <div class='header'>{{month.format('MMMM')}} Spending Overview</div>
-          <dl class='spending'>
-            <dt>Income</dt><dd class='blur'>{{this.income|usdint}}</dd>
-            <dt>Spent</dt><dd class='blur'>{{this.spent|usdint}}</dd>
-            <dt>Remaining</dt><dd class='total' :class='amountcls(this.income + this.spent)'>{{this.income + this.spent|usdint}}</dd>
-          </dl>
-          <hr/>
-          <dl class='comments'>
-            <template v-for='(amount,comment) in this.comments'>
-              <template v-if='(amount < -100) || (amount > 100)'>
-                <dt :key='comment + "_label"'>{{comment}}</dt>
-                <dd :key='comment' class='blur'>{{amount|usdint}}</dd>
+        <div id='rightpanel'>
+          <div id='monthdetails'>
+            <div class='header'>{{month.format('MMMM')}} Spending Overview</div>
+            <dl class='spending'>
+              <dt>Income</dt><dd class='blur'>{{this.income|usdint}}</dd>
+              <dt>Spent</dt><dd class='blur'>{{this.spent|usdint}}</dd>
+              <dt>Remaining</dt><dd class='total' :class='amountcls(this.income + this.spent)'>{{this.income + this.spent|usdint}}</dd>
+            </dl>
+            <hr/>
+            <dl class='comments'>
+              <template v-for='(amount,comment) in this.comments'>
+                <template v-if='(amount < -100) || (amount > 100)'>
+                  <dt :key='comment + "_label"'>{{comment}}</dt>
+                  <dd :key='comment' class='blur'>{{amount|usdint}}</dd>
+                </template>
               </template>
-            </template>
-          </dl>
+            </dl>
+          </div>
+          <canvas id='spendchart'/>
         </div>
         <div class='tablewrap'>
           <div class='clickout-detector' v-click-outside='cancelAll'>
@@ -53,11 +56,9 @@
   import * as pathify from 'vuex-pathify';
   import * as utils from '@/utils/utils';
   import * as butils from '@/components/budget/BudgetUtils';
-
   import fromPairs from 'lodash/fromPairs';
   import sortBy from 'lodash/sortBy';
   import toPairs from 'lodash/toPairs';
-
   import {TYPES} from '@/components/TableMixin';
   import dayjs from 'dayjs';
   import BudgetPopover from '@/components/budget/BudgetPopover';
@@ -73,6 +74,7 @@
       loading: false,       // True to show loading indicator
       month: dayjs(),       // Current month displayed (dayjs object)
       today: dayjs(),       // Today's month
+      transactions: [],     // List of transactions in current view
       tablerows: null,      // Row items to display
       // Smmary data
       count: 0,             // Total transactions in view
@@ -119,12 +121,18 @@
       // Get Transactions
       // Search for and update the list of displayed transactions
       getTransactions: async function() {
-        this.cancelsearch = api.cancel(this.cancelsearch);
-        var token = this.cancelsearch.token;
-        var datestr = this.month.format('MMM YYYY');
-        var params = {search:`date="${datestr}"`, limit:9999};
-        var {data} = await api.Budget.getTransactions(params, token);
-        return data.results;
+        try {
+          
+          this.cancelsearch = api.cancel(this.cancelsearch);
+          var token = this.cancelsearch.token;
+          var datestr = this.month.format('MMM YYYY');
+          var params = {search:`date="${datestr}"`, limit:9999};
+          var {data} = await api.Budget.getTransactions(params, token);
+          this.transactions = data.results;
+          
+        } catch(err) {
+          if (!api.isCancel(err)) { throw(err); }
+        }
       },
 
       // Init Columns
@@ -162,46 +170,51 @@
         return tablerows;
       },
 
+      // Populate Tablerows
+      populateTablerows: function() {
+        this.tablerows = this.initTablerows();
+        var catnames = Object.keys(this.tablerows);
+        for (var trx of this.transactions) {
+          var amount = parseFloat(trx.amount);
+          var cat = trx.category || butils.UNCATEGORIZED;
+          if (catnames.indexOf(cat.name) == -1) { continue; }
+          this.tablerows[cat.name].count += 1;
+          this.tablerows[cat.name].items.push(trx);
+          this.tablerows[cat.name].total += amount;
+          this.tablerows[butils.TOTAL.name].total += amount;
+        }
+        // Remove empty rows
+        for (var key of Object.keys(this.tablerows)) {
+          if (this.tablerows[key].total == 0) { delete this.tablerows[key]; }
+        }
+      },
+
+      // Populate Month Data
+      populateMonthData: function() {
+        this.count = 0;
+        this.income = 0;
+        this.spent = 0;
+        this.comments = new utils.DefaultDict(Number);
+        for (var trx of this.transactions) {
+          var amount = parseFloat(trx.amount);
+          var cat = trx.category || butils.UNCATEGORIZED;
+          this.count += 1;
+          this.income += cat.name == 'Income' ? amount : 0;
+          this.spent += cat.name != 'Income' ? amount : 0;
+          if (trx.comment) { this.comments[trx.comment] += amount; }
+        }
+        // Sort comments by -amount
+        this.comments = fromPairs(sortBy(toPairs(this.comments), 1));
+      },
+
       // Refresh
       // Refresh the list of transactions displayed
       refresh: async function() {
         this.loading = true;
-        try {
-          var transactions = await this.getTransactions();
-          var tablerows = this.initTablerows();
-          var categories = Object.keys(tablerows);
-          // Organize the transactions into their categories
-          this.count = 0;
-          this.income = 0;
-          this.spent = 0;
-          this.comments = new utils.DefaultDict(Number);
-          for (var trx of transactions) {
-            var amount = parseFloat(trx.amount);
-            var cat = trx.category || butils.UNCATEGORIZED;
-            if (categories.indexOf(cat.name) == -1) { continue; }
-            // Table data
-            tablerows[cat.name].count += 1;
-            tablerows[cat.name].items.push(trx);
-            tablerows[cat.name].total += amount;
-            tablerows[butils.TOTAL.name].total += amount;
-            // Summary data
-            this.count += 1;
-            this.income += cat.name == 'Income' ? amount : 0;
-            this.spent += cat.name != 'Income' ? amount : 0;
-            if (trx.comment) { this.comments[trx.comment] += amount; }
-          }
-          // Remove empty rows
-          for (var key of Object.keys(tablerows)) {
-            if (tablerows[key].total == 0) { delete tablerows[key]; }
-          }
-          this.tablerows = tablerows;
-          utils.updateHistory(this.$router, {search:this.search});
-          this.comments = fromPairs(sortBy(toPairs(this.comments), 1));
-        } catch(err) {
-          if (!api.isCancel(err)) { throw(err); }
-        } finally {
-          setTimeout(() => this.loading = false, 300);
-        }
+        await this.getTransactions();
+        this.populateTablerows();
+        this.populateMonthData();
+        setTimeout(() => this.loading = false, 100);
       },
 
       // Save
@@ -253,14 +266,15 @@
     }
 
     // Side panel
-    #monthdetails {
+    #rightpanel {
       float: right;
       width: 370px;
+    }
+    #monthdetails {
       border: 1px solid $lightbg-bg3;
       background-color:  lighten($lightbg-bg1, 2%);
       border-radius: 3px;
       padding: 1px;
-
       .header {
         font-size: 12px;
         font-weight: 600;
@@ -268,7 +282,6 @@
         line-height: 22px;
         padding: 0px 10px;
       }
-
       .spending {
         font-size: 0.95em;
         padding: 0px 10px;
@@ -278,17 +291,19 @@
         dd.ltzero { color:$lightbg-red1; font-weight:bold; }
         .total { border-top: 1px solid $lightbg-fg4; }
       }
-
       hr { margin:10px auto; }
-
-
       .comments {
         opacity: 0.8;
         padding: 0px 10px;
         dt { width: 260px; }
         dd { margin-left:270px; text-align:right; width:70px; }
       }
-
+    }
+    #spendchart {
+      background-color: rgba(0,0,0,0.05);
+      height: 250px;
+      margin-top: 20px;
+      width: 100%;
     }
 
   }
