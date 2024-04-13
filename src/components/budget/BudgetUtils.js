ddb1b6bc66924060df0a1ee229efd29b48e06b71
Michael Shepanski <mshepanski@nasuni.com>
2024-04-13 13:53:37 -0400

    Remove all vue2 and js source.

--- src/components/budget/BudgetUtils.js
+++
@@ -1,5 +0,0 @@
-import {TYPES} from '@/components/TableMixin';
-
-// Default Categories often found useful when building table structures
-export var UNCATEGORIZED = {name:'Uncategorized', budget:0};
-export var TOTAL = {name:'Total', budget:0, meta:{type:TYPES.readonly, cls:'totalrow'}};
--- src/components/notes/NotesStore.js
+++
@@ -1,6 +0,0 @@
-
-export default {
-  editing: false,             // True if we are in editing mode
-  editor: null,               // Reference to TipTap editor object
-  note: {id:null, body:''},   // Note object from the db
-};
