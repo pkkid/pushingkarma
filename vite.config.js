e68e05c4dc122806e9c3812c2f4fb8d45d2f5af0
Michael Shepanski <mshepanski@nasuni.com>
2025-01-29 16:10:13 -0500

    Remove PrimeVue references.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import {defineConfig} from 'vite'
 import {fileURLToPath, URL} from 'node:url'
-import {PrimeVueResolver} from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Components({resolvers: [PrimeVueResolver()]})
   ],
   build: {
     assetsDir: 'static',
--- vue/main.js
+++ vue/main.js
@@ -9,10 +9,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-// Mount the App after waiting for all component promises to be met. This
-// is necessary because I am searching for and loading the PrimeVue components
-// asynchronously and the app will mount before they are ready.
-// Import our styles last so they take precident.
+// Mount the App after waiting for all component promises to be met.
 import './assets/styles.css'
 import './assets/overrides.css'
 app.mount('#app')
