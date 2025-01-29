a0cc1e732ecb1d5a439ae3e61301afdfb3113114
Michael Shepanski <mshepanski@nasuni.com>
2025-01-29 16:42:54 -0500

    Fix scroll behavior

--- vue/router.js
+++ vue/router.js
@@ -2,13 +2,21 @@ import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '@/views/Home.vue'
 
 const router = createRouter({
+  mode: 'history',
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {path:'/', name:'home', component:HomeView},
     {path:'/notes', name:'notes', component: () => import('@/views/Notes.vue')},
     {path:'/budget', name:'budget', component: () => import('@/views/Budget.vue')},
     {path:'/newtab', name:'newtab', component: () => import('@/views/NewTab.vue')},
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {el:to.hash, behavior:'smooth'}
+    } else {
+      return {top:0};
+    }
+  },
 })
 
 export default router
