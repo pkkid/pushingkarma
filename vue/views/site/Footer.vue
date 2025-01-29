1262ec607f252215153518801b3457f861091557
Michael Shepanski <mshepanski@nasuni.com>
2025-01-29 14:54:05 -0500

    Remove Beufy.

--- vue/views/site/Footer.vue
+++ vue/views/site/Footer.vue
@@ -1,78 +1,78 @@
 <template>
-    <div id='footer'>
-      <div class='footerwrap'>
-        <div class='footercolumn' style='width:400px'>
-          <h4>PushingKarma</h4>
-          Site created using <a href='https://www.djangoproject.com/'>Django</a>,
-          <a href='https://www.django-rest-framework.org/'>Django Rest Framework</a>,
-          <a href='https://vuejs.org/'>Vue</a>, <a href='https://buefy.org/'>Beufy</a>,
-          and is hosted on <a href='https://www.digitalocean.com/'>Digital Ocean</a>.
-          <div style='margin-top:5px;'>
-            <i class='icon email'/> {{email}}<span style='display:none;'>+site</span>@gmail.com
-          </div>
+  <div id='footer'>
+    <div class='footerwrap'>
+      <div class='footercolumn' style='width:400px'>
+        <h4>PushingKarma</h4>
+        Site created using <a href='https://www.djangoproject.com/'>Django</a>,
+        <a href='https://www.django-rest-framework.org/'>Django Rest Framework</a>,
+        <a href='https://vuejs.org/'>Vue</a> and hosted on
+        <a href='https://www.digitalocean.com/'>Digital Ocean</a>.
+        <div style='margin-top:5px;'>
+          <i class='icon email'/> {{email}}<span style='display:none;'>+site</span>@gmail.com
         </div>
-        <div class='footercolumn' style='width:250px'>
-          <h4>Social Media</h4>
-          <div><i class='icon github'/> <a href='https://github.com/pkkid'>Github</a></div>
-          <div><i class='icon linkedin'/> <a href='https://www.linkedin.com/in/shepanski'>Linked In</a></div>
-          <div><i class='icon facebook'/> <a href='https://www.facebook.com/mshepanski'>Facebook</a></div>
-        </div>
-        <div class='footercolumn' style='width:350px'>
-          <h4>Legal</h4>
-          Copyright (c) 2020-{{year}} PushingKarma. All rights reserved.
-        </div>
-        <div style='clear:left;'/>
       </div>
+      <div class='footercolumn' style='width:250px'>
+        <h4>Social Media</h4>
+        <div><i class='icon github'/> <a href='https://github.com/pkkid'>Github</a></div>
+        <div><i class='icon linkedin'/> <a href='https://www.linkedin.com/in/shepanski'>Linked In</a></div>
+        <div><i class='icon facebook'/> <a href='https://www.facebook.com/mshepanski'>Facebook</a></div>
+      </div>
+      <div class='footercolumn' style='width:350px'>
+        <h4>Legal</h4>
+        Copyright (c) 2020-{{year}} PushingKarma. All rights reserved.
+      </div>
+      <div style='clear:left;'/>
     </div>
-  </template>
+  </div>
+</template>
 
-  <script setup>
-    import {ref} from 'vue'
-    const email = ref(atob('bWljaGFlbC5zaGVwYW5za2k='))
-    const year = new Date().getFullYear()
-  </script>
+<script setup>
+  import {ref} from 'vue'
+  const email = ref(atob('bWljaGFlbC5zaGVwYW5za2k='))
+  const year = new Date().getFullYear()
+</script>
   
-  <style>
-    #footer {
-      background-color: var(--darkbg-bg0);
-      color: var(--darkbg-fg0);
-      font-family: var(--fontfamily-article);
-      font-weight: 400;
-      font-size: 0.85rem;
-      line-height: 1.7;
-      text-align: left;
+<style>
+  #footer {
+    background-color: var(--darkbg-bg0);
+    color: var(--darkbg-fg0);
+    font-family: var(--fontfamily-article);
+    font-weight: 400;
+    font-size: 0.85rem;
+    line-height: 1.7;
+    text-align: left;
 
-      a,a:visited {
-        color: var(--darkbg-fg0);
-        text-decoration: none;
-        transition: color 0.2s ease;
-        &:hover { color: var(--darkbg-orange1); }
-      }
-      .footerwrap {
-        margin: 0px auto;
-        width: 1005px;
-      }
-      .footercolumn {
-        float:left;
-        padding: 50px;
-        position: relative;
-      }
-      h4 {
-        color: var(--darkbg-fg0);
-        font-family: var(--fontfamily-artsy);
-        font-size: 1rem;
-        font-weight: 400;
-        margin-bottom: 15px;
-        position: relative;
-        &::before {
-          background-color: var(--darkbg-orange1);
-          bottom: -3px;
-          content: ' ';
-          display: block;
-          height: 1px; 
-          position: absolute;
-          width: 30px;
-        }
+    a,a:visited {
+      color: var(--darkbg-fg0);
+      text-decoration: none;
+      transition: color 0.2s ease;
+      &:hover { color: var(--darkbg-orange1); }
+    }
+    .footerwrap {
+      margin: 0px auto;
+      width: 1005px;
+    }
+    .footercolumn {
+      float:left;
+      padding: 50px;
+      position: relative;
+    }
+    h4 {
+      color: var(--darkbg-fg0);
+      font-family: var(--fontfamily-artsy);
+      font-size: 1rem;
+      font-weight: 400;
+      margin-bottom: 15px;
+      position: relative;
+      &::before {
+        background-color: var(--darkbg-orange1);
+        bottom: -3px;
+        content: ' ';
+        display: block;
+        height: 1px; 
+        position: absolute;
+        width: 30px;
       }
     }
-  </style>
+  }
+</style>
