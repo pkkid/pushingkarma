eee68436cdf6d5cc84fefc09f0a548a9337b4f41
Michael Shepanski <mshepanski@nasuni.com>
2025-01-30 14:01:55 -0500

    I will never understand eslint config options. Overcomplicated much?

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
-import globals from 'globals'
 import path from 'node:path'
-import js from '@eslint/js'
+import pluginjs from '@eslint/js'
 import {fileURLToPath} from 'node:url'
 import {FlatCompat} from '@eslint/eslintrc'
 
@@ -8,23 +7,19 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 const compat = new FlatCompat({
     baseDirectory: dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    recommendedConfig: pluginjs.configs.recommended,
 })
 
 export default [
-  ...compat.extends('plugin:vue/vue3-essential', 'eslint:recommended'), {
-  languageOptions: {
-    globals: {...globals.node},
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
+  ...compat.extends('eslint:recommended'),
+  ...compat.extends('plugin:vue/vue3-essential'),
+  {ignores: ['pk/*']},
+  {rules: {
     'no-unused-vars': 'off',
     'semi': ['error', 'never'],
     'vue/comment-directive': 'off',
     'vue/multi-word-component-names': 'off',
     'vue/no-reserved-component-names': 'off',
     'vue/valid-v-slot': ['error', {allowModifiers:true}],
-  },
-}]
+  }}
+]
