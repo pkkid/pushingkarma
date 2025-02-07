34c333ce04d046d8fb6428eb58c4fc99f84dacec
Michael Shepanski <mshepanski@nasuni.com>
2025-02-07 11:26:31 -0500

    Add utils and allow easier imports.

---
+++ vue/utils/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import * as utils from '@/utils/utils'
+
+export const isCancel = axios.isCancel
+axios.defaults.baseURL = utils.apibase
+console.debug(`Axios.defaults.baseURL: ${axios.defaults.baseURL}`)
+
+// Cancel
+// Cancel a previously started request
+export function cancel(controller) {
+  if (controller) { controller.abort('Cancelled request') }
+  return new AbortController()
+}
+
+export const Main = {
+  genToken(signal) { return axios.post(`/main/gentoken`, {signal}) },
+  getCurrentUser(signal) { return axios.get(`/main/user`, {signal}) },
+  getGlobalVars(signal) { return axios.get(`/main/globalvars`, {signal}) },
+  login(data, signal) { return axios.post(`/main/login`, data, {signal}) },
+  logout(signal) { return axios.post(`/main/logout`, {signal}) },
+}
---
+++ vue/utils/index.js
@@ -0,0 +1,3 @@
+import * as api from './api'
+import * as utils from './utils'
+export {api, utils}
