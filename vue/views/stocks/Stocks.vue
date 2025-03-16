c8b8c1b917b8181ad2626be081883cff5311be79
Michael Shepanski <mshepanski@nasuni.com>
2025-03-15 20:39:35 -0400

    Create stocks sidepanel.

--- vue/views/stocks/Stocks.vue
+++ vue/views/stocks/Stocks.vue
@@ -1,25 +1,35 @@
 <template>
   <div id='stocks'>
-    <LayoutPaper id='budgetsettings' width='1000px'>
-      <template #content>
-        <h1>Stocks</h1>
-        <div class='chartlayout' style='margin-top:20px;'>
-          <TrendChart title='Long Term Trend' :data='longdata'/>
-          <TrendChart title='Short Term Trend' :data='shortdata'/>
-        </div>
+    <LayoutSidePanel>
+      <!-- Search -->
+      <template #panel>
+        <StocksSearch />
       </template>
-    </LayoutPaper>
+      <template #content>
+        <LayoutPaper width='1000px'>
+          <!-- Stocks Overview -->
+          <template #content>
+            <h1>Stocks</h1>
+            <div class='chartlayout' style='margin-top:20px;'>
+              <TrendChart title='Long Term Trend' :data='longdata'/>
+              <TrendChart title='Short Term Trend' :data='shortdata'/>
+            </div>
+          </template>
+        </LayoutPaper>
+      </template>
+    </LayoutSidePanel>
   </div>
 </template>
 
 <script setup>
   import {onBeforeMount, onMounted, ref} from 'vue'
   import {utils} from '@/utils'
-  import {useUrlParams} from '@/composables/useUrlParams.js'
-  import TrendChart from '@/components/TrendChart.vue'
-  import LayoutPaper from '@/components/LayoutPaper.vue'
   import {api} from '@/utils'
-
+  import LayoutPaper from '@/components/LayoutPaper.vue'
+  import LayoutSidePanel from '@/components/LayoutSidePanel.vue'
+  import StocksSearch from '@/views/stocks/StocksSearch.vue'
+  import TrendChart from '@/components/TrendChart.vue'
+  
   const shortdata = ref(null)
   const longdata = ref(null)
 
---
+++ vue/views/stocks/StocksSearch.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id='stockssearch'>
+    <!-- Search Input -->
+    <div class='searchwrap'>
+      <input ref='searchinput' class='searchinput' v-model='search' type='text' maxlength='100'
+        @keydown.enter='updateNotes' @keydown.down='focusNext' @keydown.up='focusPrev'/>
+      <span class='icon search'>search</span>
+      <transition name='fade'>
+        <span v-if='search.length' class='icon clear-search close' @click='search=""'>close</span>
+      </transition>
+    </div>
+    <!-- Pre-Canned Searches -->
+    <div ref='resultsdiv' class='results'>
+      <!-- <a href='#' class='result' v-for='note in notes' :key='note.title' :class='{selected:note.path==selected?.path}'
+        @click.prevent @click='$emit("select", note)' @keydown.enter='$emit("select", note)' 
+        @keydown.down='focusNext' @keydown.up='focusPrev'>
+        {{note.title}}
+        <div class='subtext'>{{utils.formatDate(note.mtime * 1000, 'MMM DD, YYYY')}}</div>
+      </a> -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import {onBeforeMount, onBeforeUnmount, ref} from 'vue'
+  import {api, utils} from '@/utils'
+  import hotkeys from 'hotkeys-js'
+
+  const emit = defineEmits([
+    'search'        // New search string
+  ])
+
+  var cancelctrl = null                   // Cancel controller
+  const loading = ref(false)              // True to show loading indicator
+  const search = ref('')                  // Current search string
+  const searchinput = ref(null)           // Reference to searchinput elem
+  
+  // On Before Mount
+  // Fetch initial notes to display
+  onBeforeMount(() => {
+    hotkeys('f1', 'stocks', function() {  searchinput.value.focus() })
+    hotkeys.setScope('stocks')
+  })
+
+  // On Before Mount
+  // Unbind all note hotkeys
+  onBeforeUnmount(() => {
+    hotkeys.deleteScope('stocks')
+  })
+</script>
+
+<style>
+  #stockssearch {
+  }
+</style>
