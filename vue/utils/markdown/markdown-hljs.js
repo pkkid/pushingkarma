b0a28612b7f67534a00f0d6c15054ebd90986e51
Michael Shepanski <mshepanski@nasuni.com>
2025-03-24 23:18:08 -0400

    Fix themeing. Add Vue component example.

--- vue/utils/markdown/markdown-hljs.js
+++ vue/utils/markdown/markdown-hljs.js
@@ -20,7 +20,7 @@ const wrapCodeRenderer = function(renderer) {
     const langName = token.info ? token.info.trim() : 'plaintext'
     const originalRendered = renderer(...args)
     const langLabel = `<label>${langName}</label>`
-    return `<div class="code-block">${langLabel}${originalRendered}</div>`
+    return `<div theme='gruvbox-light-hard'><div class='md-codearea hljs'>${langLabel}${originalRendered}</div></div>`
   }
 }
 
@@ -32,3 +32,26 @@ export default function(md, opts) {
   md.renderer.rules.fence = wrapCodeRenderer(md.renderer.rules.fence)
   md.renderer.rules.code_block = wrapCodeRenderer(md.renderer.rules.code_block)
 }
+
+// THIS IS HOW TO RENDER THE TEXT TO A VUE COMPONENT!!!
+// ----------------------------------------------------
+// import { createApp, h } from 'vue'
+// import CodeEditor from '@/components/CodeEditor.vue'
+// const wrapCodeRenderer = function(renderer) {
+//   return function wrappedRenderer(...args) {
+//     const [tokens, idx, options, env, self] = args
+//     const token = tokens[idx]
+//     const langName = token.info ? token.info.trim() : 'plaintext'
+//     const originalRendered = renderer(...args)
+//     const app = createApp({
+//       render: () => h(CodeEditor, {
+//         value: originalRendered,
+//         language: 'html',
+//         showLineNums: true,
+//       })
+//     })
+//     const container = document.createElement('div')
+//     app.mount(container)
+//     return container.innerHTML
+//   }
+// }
