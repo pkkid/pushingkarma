f6f297a6fd4948d9e49d1b9c930cba31f88cf772
Michael Shepanski <mshepanski@nasuni.com>
2025-03-24 23:18:27 -0400

    For now, this is my demo page.

--- vue/views/budget/BudgetYear.vue
+++ vue/views/budget/BudgetYear.vue
@@ -1,13 +1,49 @@
 <template>
   <LayoutPaper width='1200px'>
     <template #content>
-      Budget Year
+      <CodeEditor height='600px' theme='gruvbox-light-hard' :showLineNums='true' language='javascript' fontSize='12px'
+        style='height:400px; width:600px;' v-model='code'/>
     </template>
   </LayoutPaper>
 </template>
 
 <script setup>
+  import {ref, watch} from 'vue'
   import {LayoutPaper} from '@/components/Layout'
+  import CodeEditor from '@/components/CodeEditor.vue'
+
+  const code = ref(`// Main Vue Setup
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+app.use(router)
+
+// Setup Highlight.js
+// highlightjs: https://highlightjs.org/
+// https://github.com/highlightjs/vue-plugin
+import hljs from 'highlight.js/lib/core'
+import javascript from 'highlight.js/lib/languages/javascript'
+import json from 'highlight.js/lib/languages/json'
+import python from 'highlight.js/lib/languages/python'
+import sql from 'highlight.js/lib/languages/sql'
+import hljsVuePlugin from '@highlightjs/vue-plugin'
+
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('json', json)
+hljs.registerLanguage('python', python)
+hljs.registerLanguage('sql', sql)
+app.use(hljsVuePlugin)
+
+// Mount the App after waiting for all component promises to be met.
+import './assets/styles.css'
+app.mount('#app')`)
+
+  watch(code, function() {
+    console.log(code.value)
+  })
+
 </script>
 
 <style>
