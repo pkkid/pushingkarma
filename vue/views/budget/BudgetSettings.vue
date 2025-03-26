9b0a4cdf4d09c1e3db4393d35fceebe1c1c062f4
Michael Shepanski <mshepanski@nasuni.com>
2025-03-25 20:59:36 -0400

    Start adding settings.

--- vue/views/budget/BudgetSettings.vue
+++ vue/views/budget/BudgetSettings.vue
@@ -2,20 +2,33 @@
   <Modal :visible='visible' closeButton closeOnEsc @close='emit("close")'>
     <article id='budgetsettings' class='lightbg'>
       <h2 style='margin-top:0px;'>Budget Settings</h2>
+      <!-- Accounts -->
       <div v-if='accounts' class='accounts'>
         <h3>Accounts</h3>
         <Sortable group='accounts' @sort='onSortAccounts'>
           <SortableItem v-for='account in accounts.results' :key='account.id' :itemid='account.id'>
-            <Expandable>
+            <Expandable maxheight='250px'>
               <template #header>{{account.name}}</template>
-              <template #content>Hi Mom!</template>
+              <template #content>
+                <div style='margin:5px 0px 15px 0px'>
+                  <h4>Import Configuration</h4>
+                  <CodeEditor :value='account.import_config' language='json' showLineNums=true padding='8px'
+                    style='height:150px; width:100%; font-size:12px; margin-bottom:10px;'/>
+                  <div class='button-row'>
+                    <button>Save Account</button>
+                    <button>Delete Account</button>
+                  </div>
+                </div>
+              </template>
             </Expandable>
           </SortableItem>
         </Sortable>
       </div>
+      <!-- Categories -->
       <div v-if='categories' class='categories'>
+        
         <h3>Categories</h3>
-        <Sortable group='categories' @sort='onSortCategories' style='max-height:300px; overflow-y:auto;'>
+        <Sortable group='categories' @sort='onSortCategories' style='min-height:100px; max-height:300px; overflow-y:auto;'>
           <SortableItem v-for='category in categories.results' :key='category.id' :itemid='category.id'>
             {{category.name}}
           </SortableItem>
@@ -29,6 +42,7 @@
   import {ref, watchEffect} from 'vue'
   import {Sortable, SortableItem} from '@/components/Sortable'
   import {api} from '@/utils'
+  import CodeEditor from '@/components/CodeEditor.vue'
   import Expandable from '@/components/Expandable.vue'
   import Modal from '@/components/Modal.vue'
   
