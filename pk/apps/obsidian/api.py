ec27e31919ffd50890d0c5018c79dd901c0ef47c
Michael Shepanski <mshepanski@nasuni.com>
2025-03-28 18:21:30 -0400

    Update to new page scheme.

--- pk/apps/obsidian/api.py
+++ pk/apps/obsidian/api.py
@@ -5,10 +5,9 @@ from django.views.decorators.cache import cache_page
 from ninja import Router
 from ninja.decorators import decorate_view
 from ninja.errors import HttpError
-from ninja.pagination import paginate, PageNumberPagination
 from os.path import basename, exists, getmtime, join
 from typing import List, Optional
-from pk import utils
+from pk.utils import PageSchema, paginate, reverse
 from .schemas import NoteSchema
 log = logging.getLogger(__name__)
 router = Router()
@@ -32,7 +31,7 @@ def get_note(request, bucketname:str, path:str):
         with open(filepath, 'r', encoding='utf-8') as handle:
             content = handle.read()
         return dict(
-            url = utils.reverse(request, 'api:note', bucketname=bucketname, path=path),
+            url = reverse(request, 'api:note', bucketname=bucketname, path=path),
             bucket = bucketname,
             vault = bucket['vault'],
             path = path,
@@ -44,17 +43,16 @@ def get_note(request, bucketname:str, path:str):
     raise HttpError(403, 'Permission denied.')
 
 
-@router.get('/notes', response=List[NoteSchema], exclude_unset=True)
-@paginate(PageNumberPagination)
+@router.get('/notes', response=PageSchema(NoteSchema), exclude_unset=True)
 @decorate_view(cache_page(0 if settings.DEBUG else 300))
-def list_notes(request, search:Optional[str]=''):
+def list_notes(request, search:Optional[str]='', page:Optional[int]=1):
     """ Lists obsidian notes in the defined groups from settings. When searching,
         each word in the search string is counted in the content and title to give
         a score for sorting the results. The results are sorted by count and mtime,
         and returned as a list of dictionaries.
         • search (str): search query string.
     """
-    results = []
+    items = []
     search = re.sub(r'[^a-zA-Z0-9\s]', '', search[:100])
     words = search.lower().split()
     for bucketname, bucket in settings.OBSIDIAN_BUCKETS.items():
@@ -67,8 +65,8 @@ def list_notes(request, search:Optional[str]=''):
                 title = basename(filepath)[:-3]
                 score = sum(title.lower().count(word) for word in words) * 1000
                 score += sum(content.lower().count(word) for word in words)
-                results.append(dict(
-                    url = utils.reverse(request, 'api:note', bucketname=bucketname, path=path),
+                items.append(dict(
+                    url = reverse(request, 'api:note', bucketname=bucketname, path=path),
                     bucket = bucketname,
                     vault = bucket['vault'],
                     path = path,
@@ -76,5 +74,7 @@ def list_notes(request, search:Optional[str]=''):
                     mtime = int(getmtime(filepath)),
                     score = score,
                 ))
-    results = [r for r in results if r['score'] > 0] if search != '' else results
-    return sorted(results, key=lambda r: (-r['score'],-r['mtime']))
+    items = [r for r in items if r['score'] > 0] if search != '' else items
+    items = sorted(items, key=lambda r: (-r['score'],-r['mtime']))
+    data = paginate(request, items, page=page, perpage=50)
+    return data
