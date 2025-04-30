b806fc3df6499ac806ce1dc5263a44acd5e28ebd
Michael Shepanski <mshepanski@nasuni.com>
2025-04-29 22:38:07 -0400

    Better regex check.

--- vue/utils/utils.js
+++ vue/utils/utils.js
@@ -61,10 +61,15 @@ export function dedent(str) {
 
 // Format Date
 // Format a date using various format strings:
-export function formatDate(value, format) {
+export function formatDate(value, format, local=true) {
   format = format || 'MMM DD, YYYY'
   if (!value) return ''
-  const date = new Date(value)
+  // This crazy check is to properly handle timezones when
+  // the date is a string in the format YYYY-MM-DD.
+  var date = local && typeof value == 'string'
+    && /^\d{4}-\d{2}-\d{2}$/.test(value)
+    ? new Date(value + 'T00:00:00')
+    : new Date(value)
   if (isNaN(date)) return ''
   // Get the date components
   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
