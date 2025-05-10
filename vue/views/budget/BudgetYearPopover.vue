30c3adb97de988caf53d18e6f9c6f9b21d314744
Michael Shepanski <mshepanski@nasuni.com>
2025-05-10 10:44:00 -0400

    Started populating popover.

--- vue/views/budget/BudgetYearPopover.vue
+++ vue/views/budget/BudgetYearPopover.vue
@@ -1,6 +1,19 @@
 <template>
   <div v-if='showing' ref='root' id='budgetyearpopover' class='lightbg'>
-    Hi Mom!
+    {{category?.name}}
+    <div class='subtext'>{{utils.formatDate(month, 'MMMM YYYY')}}</div>
+    <table v-if='trxs'>
+      <tbody>
+        <tr v-for='(trx, index) in trxs?.items' :key='index'>
+          <td class='date'>{{utils.formatDate(trx.date, 'M/D') }}</td>
+          <td class='payee'>{{trx.payee}}</td>
+          <td class='amount'>{{trx.amount}}</td>
+        </tr>
+      </tbody><tfoot><tr>
+        <td colspan='2'></td>
+        <td class='total'>total</td>
+      </tr></tfoot>
+    </table>
   </div>
 </template>
 
@@ -15,11 +28,14 @@
   const showing = ref(false)        // True if the popover is showing
   const trxs = ref(null)            // Transactions to show
   const category = ref(null)        // Category to show
+  const month = ref(null)           // Month to show
 
   // Show Popover
   // Show the popover with the given category and month
-  const show = async function(cell, category, month, search) {
+  const show = async function(cell, cat, mon, search) {
     // console.log(`show(${category.name}, ${utils.formatDate(month, 'YYYY-MM-DD')}, "${search}")`)
+    category.value = cat
+    month.value = mon
     showing.value = true
     await nextTick()
     // Set the top position of the popover
@@ -30,8 +46,7 @@
     root.value.style.top = `${top}px`
     root.value.style.left = `${left}px`
     // Populate the popover contents
-    category.value = category
-    updateTransactions(category, month, search)
+    updateTransactions(cat, mon, search)
   }
 
   // Hide Popover
@@ -44,15 +59,15 @@
 
   // Update Transactions
   // Fetch transactions from the server
-  const updateTransactions = async function(category, month, search) {
+  const updateTransactions = async function(cat, mon, search) {
     loading.value = true
     cancelctrl = api.cancel(cancelctrl)
     try {
-      var maxdate = new Date(month)
+      var maxdate = new Date(mon)
       maxdate.setMonth(maxdate.getMonth() + 1)
       search = search || ''
-      search += ` category="${category.value.name}"`
-      search += ` date>=${utils.formatDate(month, 'YYYY-MM-DD')}`
+      search += ` category="${cat.name}"`
+      search += ` date>=${utils.formatDate(mon, 'YYYY-MM-DD')}`
       search += ` date<${utils.formatDate(maxdate, 'YYYY-MM-DD')}`
       var {data} = await api.Budget.listTransactions({search:search.trim()}, cancelctrl.signal)
       trxs.value = data
@@ -75,7 +90,7 @@
 <style>
   #budgetyearpopover {
     position: fixed;
-    width: 150px;
+    width: 300px;
     min-height: 50px;
     max-height: 300px;
     top: 0px;
@@ -84,6 +99,12 @@
     border-radius: 4px;
     box-shadow: 0 1px 3px 0 #3c40434d, 0 4px 8px 3px #3c404326;
     border: 1px solid var(--lightbg-bg4);
+    overflow: hidden;
+
+    table {
+      width: 100%;
+      font-size: 10px;
+    }
 
     .loading {
       display: flex;
