5303d3c84509806c3108797b2a8d90a26e0b1992
Michael Shepanski <mshepanski@nasuni.com>
2025-05-15 17:20:15 -0400

    Add functions first_of_month() and add_months(). Add doc strings.

--- pk/utils/utils.py
+++ pk/utils/utils.py
@@ -1,5 +1,17 @@
 # encoding: utf-8
+import datetime
+from calendar import monthrange
 from decimal import Decimal
+
+
+def add_months(dt, months):
+    """ Add months to a datetime. """
+    if not dt or not months: return dt
+    month = dt.month - 1 + months
+    year = dt.year + month // 12
+    month = month % 12 + 1
+    day = min(dt.day, monthrange(year, month)[1])
+    return dt.replace(year=year, month=month, day=day)
 
 
 def clean_amount(value):
@@ -13,13 +25,24 @@ def clean_amount(value):
     return value
 
 
+def first_of_month(dt):
+    """ Get the first day of the month for a given date. """
+    if isinstance(dt, datetime.datetime):
+        return dt.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+    elif isinstance(dt, datetime.date):
+        return dt.replace(day=1)
+    raise TypeError('dt must be a datetime.date or datetime.datetime object')
+
+
 def percent(numerator, demoniator, precision=2):
+    """ Calculate the percentage of a number. """
     if not demoniator:
         return 0
     return round(numerator / demoniator * 100, precision) if demoniator else 0
 
 
 def rgb(text, color='#aaa', reset=True):
+    """ Convert a hex color to RGB for logging or console output. """
     r,g,b = tuple(int(x * 2, 16) for x in color.lstrip('#'))
     rgbstr = f'\033[38;2;{r};{g};{b}m{text}'
     rgbstr += '\033[00m' if reset else ''
@@ -27,11 +50,12 @@ def rgb(text, color='#aaa', reset=True):
 
 
 def rget(obj, attrstr, default=None, delim='.'):
+    """ Recursively get an attribute from an object or dict. """
     try:
         parts = attrstr.split(delim, 1)
         attr = parts[0]
         attrstr = parts[1] if len(parts) == 2 else None
-        attrint = toint(attr)
+        attrint = to_int(attr)
         if isinstance(obj, dict): value = obj[attr]
         elif isinstance(obj, list) and attrint is not None: value = obj[attrint]
         elif isinstance(obj, tuple) and attrint is not None: value = obj[attrint]
@@ -42,13 +66,8 @@ def rget(obj, attrstr, default=None, delim='.'):
         return default
 
 
-def toint(value):
-    try:
-        return int(value)
-    except ValueError:
-        return None
-
 def rset(obj, attrstr, value, delim='.'):
+    """ Recursively set an attribute on an object or dict. """
     parts = attrstr.split(delim, 1)
     attr = parts[0]
     attrstr = parts[1] if len(parts) == 2 else None
@@ -57,9 +76,9 @@ def rset(obj, attrstr, value, delim='.'):
     else: obj[attr] = value
 
 
-def toback(obj, *keys):
-    """ Move keys to the back of dict. """
-    for key in keys:
-        value = obj.pop(key)
-        obj[key] = value
-    return obj
+def to_int(value):
+    """ Convert a string to an int. """
+    try:
+        return int(value)
+    except ValueError:
+        return None
