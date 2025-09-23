0635b024d69b8a1817b458655a6297976f294407
Michael Shepanski <mshepanski@nasuni.com>
2025-09-23 13:59:02 -0400

    Add time/date display styling

--- vue/views/NewTab.vue
+++ vue/views/NewTab.vue
@@ -1,37 +1,103 @@
 <template>
-  <div id='newtab'>
-    <div class='logoimg'/>
-    <div class='logotxt'>PushingKarma</div>
+  <div id='newtab-wrapper'>
+    <div id='newtab' :class='{"fullscreen": isFullscreen}'>
+      <div class='logo-container'>
+        <div class='logoimg'/>
+      </div>
+      <div class='time-container'>
+        <div class='time'>{{utils.formatDate(now, 'h:mm')}}</div>
+        <div class='date'>{{utils.formatDate(now, 'MMMM D, YYYY')}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
+<script setup>
+  import {onMounted, onUnmounted, ref} from 'vue'
+  import {utils} from '@/utils'
+
+  const now = ref()
+  const isFullscreen = ref(false)
+  var dateInterval = null
+
+  const updateDate = function() { now.value = new Date() }
+  const updateFullscreen = function() { isFullscreen.value = window.innerHeight === screen.height }
+
+  onMounted(() => {
+    updateDate()
+    updateFullscreen()
+    dateInterval = setInterval(updateDate, 1000)
+    window.addEventListener('resize', updateFullscreen)
+  })
+
+  onUnmounted(() => {
+    if (dateInterval) { clearInterval(dateInterval) }
+    window.removeEventListener('resize', updateFullscreen)
+  })
+</script>
+
 <style>
+  #newtab-wrapper {
+    background: black;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    position: relative;
+  }
+
   #newtab {
     background: url('/static/img/floral-pattern.jpg') no-repeat center center fixed;
     background-size: cover;
     height: 100vh;
     width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+    position: relative;
+    &.fullscreen {
+      height: calc(100vh - 20px);
+      width: calc(100vw - 20px);
+      animation: square-move 300s linear infinite;
+    }
 
-    .logoimg {
-      mask: url('/static/img/pk.svg') no-repeat center/contain;
-      width: 10%;
-      aspect-ratio: 1;
-      background-color: color-mix(in srgb, var(--darkbg-fg1), #000 20%);
+    .logo-container {
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+      top: 30px; left: 30px;
+      .logoimg {
+        background-color: color-mix(in srgb, var(--darkbg-fg1), #000 20%);
+        mask: url('/static/img/pk.svg') no-repeat center/contain;
+        width: 100px; height: 100px;
+      }
     }
-    .logotxt {
-      color: var(--darkbg-fg1);
-      font-family: var(--fontfamily-title);
-      font-size: 1.2vw;
-      font-weight: 700;
-      letter-spacing: 3px;
-      line-height: 30px;
-      margin-top: -0.8vw;
+
+    .time-container {
+      position: absolute;
+      top: 50%; left: 50%;
+      transform: translate(-50%, -50%);
       text-align: center;
-      text-transform: uppercase;
+      .time {
+        color: var(--darkbg-fg4);
+        font-family: var(--fontfamily-title);
+        font-size: 10rem;
+        font-weight: 400;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+      }
+      .date {
+        color: var(--darkbg-fg4);
+        font-family: var(--fontfamily-title);
+        font-size: 3rem;
+        font-weight: 400;
+        margin-top: -40px;
+        text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);
+      }
     }
+  }
+
+  @keyframes square-move {
+    0% { transform: translate(0, 0); }
+    25% { transform: translate(20px, 0); }
+    50% { transform: translate(20px, 20px); }
+    75% { transform: translate(0, 40px); }
+    100% { transform: translate(0, 0); }
   }
 </style>
