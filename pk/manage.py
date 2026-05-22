09f47426a752a1d299e24f08eef4528eb4e42c49
Michael Shepanski <mshepanski@nasuni.com>
2026-05-22 07:48:59 -0400

    Refactor variable names for consistency in manage.py

--- pk/manage.py
+++ pk/manage.py
@@ -6,26 +6,26 @@ from django.core.management import execute_from_command_line
 from django.core.management.color import color_style
 from os.path import abspath, basename, dirname, exists, expanduser, islink
 
-PROJECT_DIR = dirname(dirname(abspath(__file__)))
-ENVFILE = f'{PROJECT_DIR}/.env'
-MOUNT_CMD = expanduser('~/Projects/scripts/mount-private.py')
+PROJECTDIR = dirname(dirname(abspath(__file__)))
+ENVFILE = f'{PROJECTDIR}/.env'
+MOUNTCMD = expanduser('~/Projects/scripts/mount-private.py')
 style = color_style()
 
 
 def setup_python_path():
     """ Setup the Python path for the project. """
-    sys.path.insert(0, PROJECT_DIR)
+    sys.path.insert(0, PROJECTDIR)
     os.environ['DJANGO_SETTINGS_MODULE'] = 'pk.settings'
-    project_dir = dirname(__file__)
-    if project_dir not in sys.path:
-        sys.path.append(project_dir)
+    projectdir = dirname(__file__)
+    if projectdir not in sys.path:
+        sys.path.append(projectdir)
 
 
 def check_private_mount():
     """ Ensure private directory is mounted. """
     if not exists(ENVFILE) and 'runserver' in sys.argv:
         time.sleep(0.3)
-        os.system(MOUNT_CMD)
+        os.system(MOUNTCMD)
 
 
 def check_notes_symlinks():
@@ -34,7 +34,7 @@ def check_notes_symlinks():
     for bucket in settings.OBSIDIAN_BUCKETS:
         bucketdir = basename(settings.OBSIDIAN_BUCKETS[bucket]['path'])
         sourcedir = f'{settings.OBSIDIAN_BUCKETS[bucket]['path']}/_static'
-        destdir = f'{PROJECT_DIR}/public/static/notes/{bucketdir}'
+        destdir = f'{PROJECTDIR}/public/static/notes/{bucketdir}'
         if not islink(destdir):
             if not displayed_warning:
                 print(style.WARNING('\nWARNING: Obsidian static dir is not symlinked'))
